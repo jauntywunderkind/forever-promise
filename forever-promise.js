8b26e0019049539e3a74bcb8e038ff2cfaf9a1bb
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-07-28 17:20:44 -0400

    Check in outline

---
+++ forever-promise.js
@@ -0,0 +1,17 @@
+"use module"
+
+const function makeSigUsr(){
+	
+}
+
+const function makeUnhandledRejection(){
+
+}
+
+const function makeUncaughtException(){
+	
+}
+
+export const forever= function(){
+	
+}
